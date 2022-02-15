712593df1acdc0df35cb1b8393a5826c64aaecf7
Nick Furfaro <nfurfaro33@gmail.com>
2022-02-15 12:03:28 -0700

    fix:cleanup unused imports

--- tests/test_projects/token_ops/mod.rs
+++ tests/test_projects/token_ops/mod.rs
@@ -1,23 +1,18 @@
-use fuel_core::service::{Config, FuelService};
-use fuel_gql_client::client::FuelClient;
-use fuel_tx::{Receipt, Salt, Transaction};
-use fuel_types::ContractId;
+use fuel_tx::{Salt};
 use fuels_abigen_macro::abigen;
-use fuels_contract::contract::{CompiledContract, Contract};
-use fuels_contract::script::Script;
+use fuels_contract::contract::Contract;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 
 #[tokio::test]
 async fn mint() {
-    let rng = &mut StdRng::seed_from_u64(2321u64);
-    let salt: [u8; 32] = rng.gen();
-    let salt = Salt::from(salt);
-    // let salt = get_new_salt();
+    let salt = get_new_salt();
+
     abigen!(
         TestFuelCoinContract,
         "test_projects/token_ops/src/abi.json",
     );
+
     let compiled =
     Contract::compile_sway_contract("test_projects/token_ops", salt).unwrap();
     let (client, fuelcoin_id) = Contract::launch_and_deploy(&compiled).await.unwrap();
@@ -27,45 +22,40 @@ async fn mint() {
         value: fuelcoin_id.into(),
     };
 
-    // let workaround_salt: [u8; 32] = rng.gen();
-    let default_balance_params = ParamsGetBalance {
+    let balance_check_1 = ParamsGetBalance {
         target: fuelcoin_id.into(),
-        asset_id: c,
-        salt: 0u64,
+        asset_id: c.clone(),
+        salt: 1u64, // temp, see: https://github.com/FuelLabs/fuels-rs/issues/89
     };
 
-    let mut balance_check_params = ParamsGetBalance {
-        salt: 1u64,
-        ..default_balance_params.clone()
-    };
-
-    let mut balance_result = fuel_coin_instance.get_balance(balance_check_params).call().await.unwrap();
+    let mut balance_result = fuel_coin_instance.get_balance(balance_check_1).call().await.unwrap();
     assert_eq!(balance_result.value, 0);
 
-    let result = fuel_coin_instance
+    fuel_coin_instance
         .mint_coins(11)
         .call()
         .await
         .unwrap();
 
-    balance_check_params = ParamsGetBalance {
-        salt: 2u64,
-        ..default_balance_params.clone()
-    };
+        let balance_check_2 = ParamsGetBalance {
+            target: fuelcoin_id.into(),
+            asset_id: c.clone(),
+            salt: 2u64,
+        };
 
-    balance_result = fuel_coin_instance.get_balance(balance_check_params).call().await.unwrap();
+    balance_result = fuel_coin_instance.get_balance(balance_check_2).call().await.unwrap();
     assert_eq!(balance_result.value, 11);
-
 }
 
-async fn setup_local_node() -> FuelClient {
-    let srv = FuelService::new_node(Config::local_node()).await.unwrap();
-    FuelClient::from(srv.bound_address)
+
+fn get_new_salt() -> Salt {
+    let rng = &mut StdRng::seed_from_u64(2321u64);
+    let salt: [u8; 32] = rng.gen();
+    let salt = Salt::from(salt);
+    salt
 }
 
-// fn get_new_salt() -> Salt {
-//     let rng = &mut StdRng::seed_from_u64(2321u64);
-//     let salt: [u8; 32] = rng.gen();
-//     let salt = Salt::from(salt);
-//     salt
+// async fn setup_local_node() -> FuelClient {
+//     let srv = FuelService::new_node(Config::local_node()).await.unwrap();
+//     FuelClient::from(srv.bound_address)
 // }

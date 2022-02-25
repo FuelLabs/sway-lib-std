29932940836852155c89e65ce8693e807086db03
Nick Furfaro <nfurfaro33@gmail.com>
2022-02-25 11:23:18 -0700

    test:add test file and empty tests

---
+++ tests/test_projects/registers/mod.rs
@@ -0,0 +1,38 @@
+use fuel_tx::Salt;
+use fuels_abigen_macro::abigen;
+use fuels_contract::contract::Contract;
+
+#[tokio::test]
+async fn call_zero() {
+
+}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}
+
+async fn <FUNC> () {}

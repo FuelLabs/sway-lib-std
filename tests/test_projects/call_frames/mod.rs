e5248cc6eba099c939551f9723b6470eb4a83691
Nick Furfaro <nfurfaro33@gmail.com>
2022-03-04 12:45:30 -0700

    test:add first test

--- tests/test_projects/call_frames/mod.rs
+++ tests/test_projects/call_frames/mod.rs
@@ -4,19 +4,33 @@ use fuels_abigen_macro::abigen;
 use fuels_contract::contract::Contract;
 use fuels_signers::provider::Provider;
 
+abigen!(
+    CallFramesTestContract,
+    "test_projects/call_frames/out/debug/call_frames-abi.json"
+);
+
 #[tokio::test]
-async fn can_get_overflow() {
-    abigen!(
-        CallFrameTestContract,
-        "test_projects/registers/out/debug/call_frames-abi.json",
-    );
+async fn can_get_contract_id() {
     let salt = Salt::from([0u8; 32]);
     let compiled = Contract::compile_sway_contract("test_projects/call_frames", salt).unwrap();
     let client = Provider::launch(Config::local_node()).await.unwrap();
     let id = Contract::deploy(&compiled, &client).await.unwrap();
-    let instance = CallFrameTestContract::new(id.to_string(), client);
+    let instance = CallFramesTestContract::new(id.to_string(), client);
 
-    let result = instance.get_overflow().call().await.unwrap();
+    println!("Contract deployed at: {:#?}", id);
 
-    assert_eq!(result.value, 0);
+    let c = callframestestcontract_mod::ContractId {
+        value: id.into(),
+    };
+
+    let result = instance
+        .get_id()
+        .call()
+        .await
+        .unwrap();
+    // println!("result: {:#?}", result);
+
+  // todo: log out the receipts on the fuels-rs side
+
+    assert_eq!(result.value, c);
 }

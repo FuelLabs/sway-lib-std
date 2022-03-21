16da36210af912754039d63b1d00803eaa608206
Nick Furfaro <nfurfaro33@gmail.com>
2022-03-21 15:18:33 -0600

    test: finish script test

--- tests/test_projects/contract_id_type/mod.rs
+++ tests/test_projects/contract_id_type/mod.rs
@@ -1,14 +1,15 @@
 use fuel_core::service::Config;
-use fuel_tx::Salt;
-use fuels_abigen_macro::abigen;
-use fuels_contract::contract::Contract;
 use fuels_signers::provider::Provider;
+use fuels_contract::script::Script;
+use fuel_types::ContractId;
+use fuel_tx::{Receipt, Transaction};
+use std::fs::read;
+
 
 
 #[tokio::test]
 async fn contract_id_eq_implementation() {
-    let salt = Salt::from([0u8; 32]);
-    let compiled = Script::compile_sway_script("test_projects/contract_id_type").unwrap();
+    let bin = std::fs::read("test_projects/contract_id_type/out/debug/contract_id_type.bin");
     let client = Provider::launch(Config::local_node()).await.unwrap();
 
     let tx = Transaction::Script {
@@ -17,7 +18,7 @@ async fn contract_id_eq_implementation() {
         maturity: 0,
         byte_price: 0,
         receipts_root: Default::default(),
-        script: compiled.raw, // Here we pass the compiled script into the transaction
+        script: bin.unwrap(), // Here we pass the compiled script into the transaction
         script_data: vec![],
         inputs: vec![],
         outputs: vec![],

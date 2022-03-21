611adfe9d8ff23921ca95c65e64da82d68830396
Nick Furfaro <nfurfaro33@gmail.com>
2022-03-21 14:43:01 -0600

    test:wip on sscript test

--- tests/test_projects/contract_id_type/mod.rs
+++ tests/test_projects/contract_id_type/mod.rs
@@ -4,6 +4,10 @@ use fuels_abigen_macro::abigen;
 use fuels_contract::contract::Contract;
 use fuels_signers::provider::Provider;
 
-async fn contract_id_eq_implementation() {
 
+#[tokio::test]
+async fn contract_id_eq_implementation() {
+    let salt = Salt::from([0u8; 32]);
+    let compiled = Script::compile_sway_script("test_projects/contract_id_type").unwrap();
+    let client = Provider::launch(Config::local_node()).await.unwrap();
 }

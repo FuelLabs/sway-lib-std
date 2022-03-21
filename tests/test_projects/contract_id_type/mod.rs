bc89c950f002f9f3d29c651bfe74e5db49cb0b24
Nick Furfaro <nfurfaro33@gmail.com>
2022-03-21 14:33:44 -0600

    test:add test project for contractId eq implementation

---
+++ tests/test_projects/contract_id_type/mod.rs
@@ -0,0 +1,9 @@
+use fuel_core::service::Config;
+use fuel_tx::Salt;
+use fuels_abigen_macro::abigen;
+use fuels_contract::contract::Contract;
+use fuels_signers::provider::Provider;
+
+async fn contract_id_eq_implementation() {
+
+}
--- tests/test_projects/harness.rs
+++ tests/test_projects/harness.rs
@@ -1,3 +1,4 @@
 // Add test modules here:
 mod registers;
 mod token_ops;
+mod contract_id_type;

c3701b9b6768c4749af8f180e5d21282d706f985
Nick Furfaro <nfurfaro33@gmail.com>
2022-03-21 14:57:13 -0600

    test:add script call draft

--- tests/test_projects/contract_id_type/mod.rs
+++ tests/test_projects/contract_id_type/mod.rs
@@ -10,4 +10,30 @@ async fn contract_id_eq_implementation() {
     let salt = Salt::from([0u8; 32]);
     let compiled = Script::compile_sway_script("test_projects/contract_id_type").unwrap();
     let client = Provider::launch(Config::local_node()).await.unwrap();
+
+    let tx = Transaction::Script {
+        gas_price: 0,
+        gas_limit: 1_000_000_000,
+        maturity: 0,
+        byte_price: 0,
+        receipts_root: Default::default(),
+        script: compiled.raw, // Here we pass the compiled script into the transaction
+        script_data: vec![],
+        inputs: vec![],
+        outputs: vec![],
+        witnesses: vec![vec![].into()],
+        metadata: None,
+    };
+
+    let script = Script::new(tx);
+    let receipts = script.call(&client).await.unwrap();
+
+    let expected_receipt = Receipt::Return {
+        id: ContractId::new([0u8; 32]),
+        val: 1,
+        pc: receipts[0].pc().unwrap(),
+        is: 464,
+    };
+
+    assert_eq!(expected_receipt, receipts[0]);
 }

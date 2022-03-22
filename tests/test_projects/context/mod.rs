32fcfccb3d44b16285ce9cf18ab10a493e39f165
Nick Furfaro <nfurfaro33@gmail.com>
2022-03-22 09:33:18 -0600

    style: cargo fmt

--- tests/test_projects/context/mod.rs
+++ tests/test_projects/context/mod.rs
@@ -1,48 +1,80 @@
-use fuel_tx::{Salt, ContractId};
+use fuel_core::service::Config;
+use fuel_tx::{ContractId, Salt};
 use fuels_abigen_macro::abigen;
 use fuels_contract::contract::Contract;
 use fuels_contract::parameters::TxParameters;
-use fuel_core::service::Config;
-use fuels_signers::{{provider::Provider}, LocalWallet};
-use fuels_signers::util::test_helpers::{setup_address_and_coins, setup_test_provider, setup_test_provider_and_wallet};
 use fuels_core::constants::{DEFAULT_COIN_AMOUNT, NATIVE_ASSET_ID};
+use fuels_signers::util::test_helpers::{
+    setup_address_and_coins, setup_test_provider, setup_test_provider_and_wallet,
+};
+use fuels_signers::{provider::Provider, LocalWallet};
 
-abigen!(TestContextContract, "test_projects/context/out/debug/context-abi.json",);
-abigen!(TestContextCallerContract, "test_artifacts/context_caller_contract/out/debug/context_caller_contract-abi.json",);
-abigen!(FuelCoin, "test_projects/token_ops/out/debug/token_ops-abi.json");
+abigen!(
+    TestContextContract,
+    "test_projects/context/out/debug/context-abi.json",
+);
+abigen!(
+    TestContextCallerContract,
+    "test_artifacts/context_caller_contract/out/debug/context_caller_contract-abi.json",
+);
+abigen!(
+    FuelCoin,
+    "test_projects/token_ops/out/debug/token_ops-abi.json"
+);
 
-async fn get_context_instance() -> (TestContextContract, ContractId, testcontextcontract_mod::ContractId) {
+async fn get_context_instance() -> (
+    TestContextContract,
+    ContractId,
+    testcontextcontract_mod::ContractId,
+) {
     let salt = Salt::from([0u8; 32]);
-    let compiled = Contract::load_sway_contract("test_projects/context/out/debug/context.bin", salt).unwrap();
+    let compiled =
+        Contract::load_sway_contract("test_projects/context/out/debug/context.bin", salt).unwrap();
     let (pk, coins) = setup_address_and_coins(1, DEFAULT_COIN_AMOUNT);
     let client = Provider::launch(Config::local_node()).await.unwrap();
     let provider = Provider::new(client);
     let wallet = LocalWallet::new_from_private_key(pk, provider.clone()).unwrap();
 
-    let contract_id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default()).await.unwrap();
-    let instance = TestContextContract::new(contract_id.to_string(), provider.clone(), wallet.clone());
-    let sway_id = testcontextcontract_mod::ContractId { value: contract_id.into() };
+    let contract_id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default())
+        .await
+        .unwrap();
+    let instance =
+        TestContextContract::new(contract_id.to_string(), provider.clone(), wallet.clone());
+    let sway_id = testcontextcontract_mod::ContractId {
+        value: contract_id.into(),
+    };
 
-    (instance, contract_id, sway_id,)
+    (instance, contract_id, sway_id)
 }
 
-async fn get_caller_instance() -> (TestContextCallerContract, ContractId, testcontextcallercontract_mod::ContractId) {
+async fn get_caller_instance() -> (
+    TestContextCallerContract,
+    ContractId,
+    testcontextcallercontract_mod::ContractId,
+) {
     let salt = Salt::from([0u8; 32]);
-    let compiled = Contract::load_sway_contract("test_projects/context/out/debug/context.bin", salt).unwrap();
+    let compiled =
+        Contract::load_sway_contract("test_projects/context/out/debug/context.bin", salt).unwrap();
     let (pk, coins) = setup_address_and_coins(1, DEFAULT_COIN_AMOUNT);
     let client = Provider::launch(Config::local_node()).await.unwrap();
     let provider = Provider::new(client);
     let wallet = LocalWallet::new_from_private_key(pk, provider.clone()).unwrap();
 
-    let compiled = Contract::load_sway_contract("test_artifacts/context_caller_contract/out/debug/context_caller_contract.bin", salt).unwrap();
-    let contract_id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default()).await.unwrap();
+    let compiled = Contract::load_sway_contract(
+        "test_artifacts/context_caller_contract/out/debug/context_caller_contract.bin",
+        salt,
+    )
+    .unwrap();
+    let contract_id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default())
+        .await
+        .unwrap();
     let instance = TestContextCallerContract::new(contract_id.to_string(), provider, wallet);
-    let sway_id = testcontextcallercontract_mod::ContractId { value: contract_id.into() };
+    let sway_id = testcontextcallercontract_mod::ContractId {
+        value: contract_id.into(),
+    };
 
-    (instance, contract_id, sway_id,)
-
+    (instance, contract_id, sway_id)
 }
-
 
 #[tokio::test]
 async fn can_get_this_balance() {
@@ -50,7 +82,12 @@ async fn can_get_this_balance() {
     let (caller_instance, caller_id, caller_sway_id) = get_caller_instance().await;
 
     let send_amount = 42;
-    caller_instance.call_get_this_balance_with_coins(send_amount, caller_sway_id).set_contracts(&[context_id]).call().await.unwrap();
+    caller_instance
+        .call_get_this_balance_with_coins(send_amount, caller_sway_id)
+        .set_contracts(&[context_id])
+        .call()
+        .await
+        .unwrap();
 
     let result = context_instance
         .get_this_balance(context_sway_id)
@@ -93,7 +130,12 @@ async fn can_get_msg_value() {
     let (caller_instance, caller_id, caller_sway_id) = get_caller_instance().await;
     let send_amount = 11;
 
-    let result = caller_instance.call_get_amount_with_coins(send_amount, caller_sway_id).set_contracts(&[context_id]).call().await.unwrap();
+    let result = caller_instance
+        .call_get_amount_with_coins(send_amount, caller_sway_id)
+        .set_contracts(&[context_id])
+        .call()
+        .await
+        .unwrap();
     assert_eq!(result.value, send_amount);
 }
 
@@ -103,7 +145,12 @@ async fn can_get_msg_id() {
     let (caller_instance, caller_id, caller_sway_id) = get_caller_instance().await;
     let send_amount = 42;
 
-    let result = caller_instance.call_get_asset_id_with_coins(send_amount, caller_sway_id).set_contracts(&[context_id]).call().await.unwrap();
+    let result = caller_instance
+        .call_get_asset_id_with_coins(send_amount, caller_sway_id)
+        .set_contracts(&[context_id])
+        .call()
+        .await
+        .unwrap();
     assert_eq!(result.value, caller_sway_id);
 }
 
@@ -113,7 +160,12 @@ async fn can_get_msg_gas() {
     let (caller_instance, caller_id, caller_sway_id) = get_caller_instance().await;
     let send_amount = 11;
 
-    let result = caller_instance.call_get_amount_with_coins(send_amount, caller_sway_id).set_contracts(&[context_id]).call().await.unwrap();
+    let result = caller_instance
+        .call_get_amount_with_coins(send_amount, caller_sway_id)
+        .set_contracts(&[context_id])
+        .call()
+        .await
+        .unwrap();
 
     assert_eq!(result.value, 11);
 }
@@ -124,7 +176,12 @@ async fn can_get_global_gas() {
     let (caller_instance, caller_id, caller_sway_id) = get_caller_instance().await;
     let send_amount = 11;
 
-    let result = caller_instance.call_get_amount_with_coins(send_amount, caller_sway_id).set_contracts(&[context_id]).call().await.unwrap();
+    let result = caller_instance
+        .call_get_amount_with_coins(send_amount, caller_sway_id)
+        .set_contracts(&[context_id])
+        .call()
+        .await
+        .unwrap();
 
     assert_eq!(result.value, 11);
 }

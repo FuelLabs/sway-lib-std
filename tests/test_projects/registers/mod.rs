3cfd725ae3b7558f24d3424c129b8f990e791a58
Nick Furfaro <nfurfaro33@gmail.com>
2022-03-22 15:49:51 -0600

    cleanup: remove compiler warnings

--- tests/test_projects/registers/mod.rs
+++ tests/test_projects/registers/mod.rs
@@ -1,4 +1,3 @@
-use fuel_core::service::{Config, FuelService};
 use fuel_tx::Salt;
 use fuel_vm::consts::VM_MAX_RAM;
 use fuels_abigen_macro::abigen;
@@ -13,26 +12,25 @@ abigen!(
 
 
 // Compile contract, create node and deploy contract, returning TestRegistersContract contract instance
-// TO DO : 
+// TO DO :
 //    -  Ability to return any type of Contract.
 //    -  Return a result
-async fn deploy_testRegisters_instance() -> TestRegistersContract {
+async fn deploy_test_registers_instance() -> TestRegistersContract {
 
     let salt = Salt::from([0u8; 32]);
     let compiled =
         Contract::load_sway_contract("test_projects/registers/out/debug/registers.bin", salt)
             .unwrap();
-    let server = FuelService::new_node(Config::local_node()).await.unwrap();
     let (provider, wallet) = test_helpers::setup_test_provider_and_wallet().await;
     let id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default()).await.unwrap();
-    
+
     TestRegistersContract::new(id.to_string(), provider, wallet)
 }
 
 #[tokio::test]
 async fn can_get_overflow() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_overflow().call().await.unwrap();
     assert_eq!(result.value, 0);
 }
@@ -40,23 +38,23 @@ async fn can_get_overflow() {
 #[tokio::test]
 async fn can_get_program_counter() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_program_counter().call().await.unwrap();
     assert!(is_within_range(result.value));
 }
 
 #[tokio::test]
 async fn can_get_stack_start_ptr() {
-    
-    let instance = deploy_testRegisters_instance().await;
+
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_stack_start_ptr().call().await.unwrap();
     assert!(is_within_range(result.value));
 }
 
 #[tokio::test]
 async fn can_get_stack_ptr() {
-    
-    let instance = deploy_testRegisters_instance().await;
+
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_stack_ptr().call().await.unwrap();
     assert!(is_within_range(result.value));
 }
@@ -64,7 +62,7 @@ async fn can_get_stack_ptr() {
 #[tokio::test]
 async fn can_get_frame_ptr() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_frame_ptr().call().await.unwrap();
     assert!(is_within_range(result.value));
 }
@@ -72,7 +70,7 @@ async fn can_get_frame_ptr() {
 #[tokio::test]
 async fn can_get_heap_ptr() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_heap_ptr().call().await.unwrap();
     assert!(is_within_range(result.value));
 }
@@ -80,7 +78,7 @@ async fn can_get_heap_ptr() {
 #[tokio::test]
 async fn can_get_error() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_error().call().await.unwrap();
     assert_eq!(result.value, 0);
 }
@@ -88,7 +86,7 @@ async fn can_get_error() {
 #[tokio::test]
 async fn can_get_global_gas() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_global_gas().call().await.unwrap();
     assert!(is_within_range(result.value));
 }
@@ -96,7 +94,7 @@ async fn can_get_global_gas() {
 #[tokio::test]
 async fn can_get_context_gas() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_context_gas().call().await.unwrap();
     assert!(is_within_range(result.value));
 }
@@ -104,7 +102,7 @@ async fn can_get_context_gas() {
 #[tokio::test]
 async fn can_get_balance() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_balance().call().await.unwrap();
     assert_eq!(result.value, 0);
 }
@@ -112,7 +110,7 @@ async fn can_get_balance() {
 #[tokio::test]
 async fn can_get_instrs_start() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_instrs_start().call().await.unwrap();
     assert!(is_within_range(result.value));
 }
@@ -120,7 +118,7 @@ async fn can_get_instrs_start() {
 #[tokio::test]
 async fn can_get_return_value() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_return_value().call().await.unwrap();
     assert_eq!(result.value, 0);
 }
@@ -128,7 +126,7 @@ async fn can_get_return_value() {
 #[tokio::test]
 async fn can_get_return_length() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_return_length().call().await.unwrap();
     assert_eq!(result.value, 0);
 }
@@ -136,7 +134,7 @@ async fn can_get_return_length() {
 #[tokio::test]
 async fn can_get_flags() {
 
-    let instance = deploy_testRegisters_instance().await;
+    let instance = deploy_test_registers_instance().await;
     let result = instance.get_flags().call().await.unwrap();
     assert_eq!(result.value, 0);
 }
